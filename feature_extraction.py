5884b27abcb6965dfea11f12ae83fb14054dd55f
damidirad <85406408+damidirad@users.noreply.github.com>
2024-06-10 19:50:07 +0200

    Basic feature extraction

---
+++ feature_extraction.py
@@ -0,0 +1,27 @@
+import json
+import datetime
+import re
+
+logs = open("data/eod_logs_job_only_cleaned_up.json")
+logs_info = []
+timestamps = []
+date_format = "%Y-%m-%dT%H:%M:%S.%f%z"
+
+for log in logs:
+    log_dict = json.loads(log)
+    logs_info.append(log_dict)
+    timestamps.append(datetime.datetime.strptime(log_dict['timestamp'], date_format))
+
+durations = []
+pattern = r'name=(.*?),.*?duration=PT(.*?),'
+for log in logs_info:
+    match = re.search(pattern, log['line'])
+    if match:
+        name = match.group(1)
+        duration = match.group(2)
+        result = {
+            "name": name,
+            "duration": duration,}
+        durations.append(result)
+
+print(durations)
